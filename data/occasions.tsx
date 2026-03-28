66740ce45a329ede664256f47dd263deaa57098d
VEERUKUMAR-09 <veerukumar5110@gmail.com>
2026-03-28 20:53:28 +0530

    Updated occasions.tsx with new implementation

---
+++ data/occasions.tsx
@@ -0,0 +1,42 @@
+// data/occasions.tsx
+
+export const occasionsData: any = {
+  "Wedding": [
+    { t: "Bridal", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/bridal.jpg?v=1756854096", path: "/category/wedding/bridal" },
+    { t: "Engagement", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Engagement.jpg?v=1756854096", path: "/category/wedding/engagement" },
+    { t: "Haldi", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Haldi.jpg?v=1756854096", path: "/category/wedding/haldi" },
+    { t: "Mehendi", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Mehendi.jpg?v=1756854096", path: "/category/wedding/mehendi" },
+    { t: "Sangeet", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Sangeet_8897a38e-0c8b-45e7-9e6a-80f15be21bd4.jpg?v=1760433813", path: "/category/wedding/sangeet" },
+    { t: "Reception", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Reception.jpg?v=1756854096", path: "/category/wedding/reception" },
+    { t: "Bridesmaid", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Bridesmaid.jpg?v=1756854097", path: "/category/wedding/bridesmaid" }
+  ],
+  "Festive": [
+    { t: "Diwali", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Diwali_80be35c3-0a55-4164-982f-dda2cbe24219.jpg?v=1758519835", path: "/category/festive/diwali" },
+
+    { t: "Onam", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Onam.jpg?v=1756854786", path: "/category/festive/onam" },
+
+    { t: "Pongal", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Pongal.jpg?v=1756854786", path: "/category/festive/pongal" },
+
+      { t: "Varalakshmi Vratam", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Varamalakshmi.jpg?v=1756854786", path: "/category/festive/navratri" }
+  ],
+  "Events": [
+    { t: "Family Gatherings", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Family_Gatherings.jpg?v=1756855186", path: "/category/events/family-gatherings" },
+
+     { t: "Party Cocktail", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Party_Cocktail.jpg?v=1756855186", path: "/category/events/party-cocktail" },
+
+     { t: "Formal Gathering", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Formal_Gathering.jpg?v=1756855186", path: "/category/formal gatherings" },
+
+     { t: "Cultural Event", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Cultural_event.jpg?v=1756855186", path: "/category/events/cultural event" },
+ 
+  ],
+  "Everyday": [
+    { t: "Workwear", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Workwear.jpg?v=1756855488", path: "/category/everyday/work-wear" },
+
+    { t: "Summer Cottons", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Casual.png?v=1772602469", path: "/category/everyday/summer cottons" },
+  ],
+  "Seasonal": [
+    { t: "Summer Sarees", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Summer_sarees.jpg?v=1756855612", path: "/category/seasonal/summer-sarees" },
+
+    { t: "Winter Sarees", img: "https://cdn.shopify.com/s/files/1/0944/8862/5451/files/Winters_Sarees.jpg?v=1756855612", path: "/category/seasonal/winter-sarees" },
+  ]
+};
